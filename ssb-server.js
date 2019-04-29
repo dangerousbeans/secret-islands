a61e26c4eb003ef1418532afe71d8c93baea948c
dangerousbeans <joran.k@gmail.com>
2019-04-30 10:39:31 +1200

    server config: allow connections

--- ssb-server.js
+++ ssb-server.js
@@ -1,19 +1,28 @@
 var Server = require('ssb-server')
 var ssbKeys = require('ssb-keys')
 var Config = require('ssb-config/inject')
+var host = "localhost"
 var config = Config('ssb', {
+	host: "localhost",
+	blobsPort: 8989,
 	connections: { 
 	  "incoming": {
-	   "ws": [{
-	      "scope": ["device"],
-	      "port": 9000,
-	      "transform": "noauth",
+	  	"ws": [{
+	   		"host": host,
+	   	  "scope": ["public", "local"],
+	      "port": 8989,
+	      "transform": "shs",
 	      "http": true
 	    }]
 	  },
 	  "outgoing": {
 	    "net": [{ "transform": "shs" }]
 	  }
+	},
+	permissions:
+	{
+		allow: null,
+		deny: null
 	},
 	logging:
 	{
@@ -29,13 +38,19 @@ config.keys = keys
 
 // add plugins
 Server
-  // .use(require('ssb-server/plugins/master'))
-  .use(require('ssb-gossip')) // this causes same error
+  // .use(require('./ssb-trusting'))
+  // .use(require('./ssb-clingy'))
+  // .use(require('ssb-server/plugins/logging'))
+  .use(require('ssb-server/plugins/plugins'))
+  .use(require('ssb-gossip'))
   .use(require('ssb-replicate'))
   .use(require('ssb-backlinks'))
   .use(require('ssb-query'))
   .use(require('ssb-blobs'))
   .use(require('ssb-ws'))
+  .use(require('ssb-ebt'))
+  .use(require('ssb-ooo'))
+  
 
 
 var server = Server(config)
