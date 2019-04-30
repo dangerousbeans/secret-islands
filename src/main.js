3036082e989f559f4011b5c20d1c3519adae3f32
dangerousbeans <joran.k@gmail.com>
2019-04-30 13:01:16 +1200

    use public pub as default (will add localhost switching later)

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import ssbclient from 'vue-ssb'
 Vue.use(ssbclient, 
 	{ 
 		keys: localStorage.keys,
-		remote: "ws://localhost:8989~shs:QdWHHh6xi1OlaaRR9GjjXDp+2IiQZElhy+GGNN4zmgY=",
+		remote: "ws://ssb.guild.land:8989~shs:QdWHHh6xi1OlaaRR9GjjXDp+2IiQZElhy+GGNN4zmgY=",
 		key: "QdWHHh6xi1OlaaRR9GjjXDp+2IiQZElhy+GGNN4zmgY=.ed25519" // public key of server
 	})
 
