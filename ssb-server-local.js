9035b2c4e9f87a7065134782bbba77c67055866b
Joran <joran.k@gmail.com>
2019-05-22 09:14:54 +1200

    local ssb server setup

---
+++ ssb-server-local.js
@@ -0,0 +1,61 @@
+var Server = require('ssb-server')
+var ssbKeys = require('ssb-keys')
+var Config = require('ssb-config/inject')
+var host = "localhost"
+var config = Config('ssb', {
+	blobsPort: 9000,
+	local: true,
+	
+	permissions:
+	{
+		allow: null,
+		deny: null
+	},
+	logging:
+	{
+		level: "info"
+	},
+  connections: { incoming: {
+      net: [{
+        host: '::',
+        port: 8008,
+        scope: ['device', 'local', 'public'],
+        transform: 'shs',
+
+      }],
+      ws: [{
+        host: '::',
+        port: 8989,
+        scope: ['device', 'local', 'public'],
+        transform: 'shs',
+        "http": true
+      }]
+    }}
+})
+
+var keys = ssbKeys.loadOrCreateSync("secret")
+
+config.keys = keys
+
+
+// add plugins
+Server
+  .use(require('./ssb-trusting'))
+  // .use(require('./ssb-clingy'))
+  // .use(require('ssb-server/plugins/logging'))
+  .use(require('ssb-server/plugins/plugins'))
+  .use(require('ssb-gossip'))
+  .use(require('ssb-replicate'))
+  .use(require('ssb-friends'))
+  .use(require('ssb-backlinks'))
+  .use(require('ssb-query'))
+  .use(require('ssb-blobs'))
+  .use(require('ssb-links'))
+  .use(require('ssb-ws'))
+  .use(require('ssb-ebt'))
+  .use(require('ssb-ooo'))
+  .use(require('ssb-server/plugins/local')) 
+
+
+var server = Server(config)
+
