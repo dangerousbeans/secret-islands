40e6a1d3774aec8c837152108612da619c352d42
Joran <joran.k@gmail.com>
2019-06-12 23:14:53 +1200

    CLINGY

--- ssb-clingy.js
+++ ssb-clingy.js
@@ -1,10 +1,29 @@
 // Clingy Plugin
 //
 // Will follow any connecting id unless already following
-// module.exports = function (api, opts) {
-//   api.auth.hook(function (fn, args) {
-//     var id = args[0]
-//     var cb = args[1]
-//     cb(null, {allow: null, deny: null})
-//   })
-// }
+module.exports = function (api, opts) {
+  api.auth.hook(function (fn, args) {
+  	var connecting_id = args[0]
+    var our_id = api.whoami().id
+    var cb = args[1]
+
+    api.friends.isFollowing({source: our_id, dest: connecting_id}, function(err, following)
+    {
+    	if(following)
+    	{
+    		cb(null, {allow: null, deny: null})
+    	}
+    	else
+    	{
+    		// Yay more friendssss
+    		api.publish({
+    			type: "contact",
+    			contact: connecting_id,
+    			following: true
+    		}, function(){
+    			cb(null, {allow: null, deny: null})
+    		})
+    	}
+    })    
+  })
+}
