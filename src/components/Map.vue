f44a1b1f6cd4c40c5c0e11ef47c3d734be255d27
Joran <joran.k@gmail.com>
2019-08-14 18:14:40 +1200

    map display fix

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -336,7 +336,7 @@ export default {
           return d.tags.join(', ');
         })
         .attr('transform', function(d) {
-          return 'translate(' + ( d.i * 35 ) + ',' + ( d.j * 30 ) + ')';
+          return 'translate(' + ( d.i * 34.64 ) + ',' + ( d.j * 30 ) + ')';
         })
     },
 
